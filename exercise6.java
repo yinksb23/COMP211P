ce95c06672cd782cf6d18440dd13059ad668fbee
Yinka Banjo <adeyinka.banjo.12@ucl.ac.uk>
2016-11-29 17:39:37 +0000

    Add existing files

---
+++ exercise6.java
@@ -0,0 +1,47 @@
+package LabsWeek3;
+import java.util.Scanner; 
+
+public class exercise6 {
+	
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		int userYear; 
+		int leapYear; 
+		
+		System.out.println("Please enter a year: ");
+		userYear = input.nextInt(); 
+		
+		leapYear = userYear%100; 
+		
+		if(leapYear == 0)
+		{
+			leapYear = userYear%400; 
+			if (leapYear == 0)
+			{
+				System.out.println("Your submitted year " + userYear + " is a leap year!");
+			}
+			else
+			{
+				System.out.println("Unfortunately, your submitted year " + userYear + " is not a leap year");
+			}
+		}
+		else
+		{
+			leapYear = userYear%4; 
+			if (leapYear == 0)
+			{
+				System.out.println("Your submitted year " + userYear + " is a leap year!");
+			}
+			else
+			{
+				System.out.println("Unfortunately, your submitted year " + userYear + " is not a leap year");
+			}
+		}
+		
+		input.close();
+		
+		
+	}
+
+}
